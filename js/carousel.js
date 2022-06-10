7631b23e57eed6595bcb8ab7e8fe33690d170ae2
Mohamamd-Haikal <mohammad.haikal@htu..edu.jo>
2022-06-11 02:00:45 +0300

    .

--- js/carousel.js
+++
@@ -1,6 +0,0 @@
-var myCarousel1 = document.querySelector("#carousel1");
-var carousel1 = new bootstrap.Carousel(myCarousel1, {
-  interval: 3000,
-  touch: true,
-  pause: false,
-});
--- modules/ajax/rate.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../DB.php');
-include('../Rating.php');
-
-if (isset($_POST['rate'])) {
-    Rating::rate($_POST['brandId'], $_POST['rate']);
-}
